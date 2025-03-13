9f1487bab732dc3b46c058d8f46fe18fe8fabf9a
shalini <shalini18031999@gmail.com>
2025-03-13 11:15:14 +0530

    first modified

--- demo1.py
+++ demo1.py
@@ -1,1 +1,7 @@
-print("hello world")
+a = int(input("Enter value of a: "))  # Convert input to integer
+b = int(input("Enter value of b: "))  # Convert input to integer
+
+if a > b:
+    print(a)
+else:
+    print(b)
